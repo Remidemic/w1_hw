bc8cecd0b55e545fed661732f798bfc70cc7ed0a
Remidemic <105177924+Remidemic@users.noreply.github.com>
2022-08-09 20:54:04 -0400

    Add files via upload

---
+++ w1d2hw.js
@@ -0,0 +1,47 @@
+var AA = 10
+var BB = 91
+var CC = (AA + BB)
+
+console.log(CC)
+
+var DD = 33
+var EE = 8765309
+
+console.log(AA + BB - CC * DD / EE)
+
+
+
+
+
+
+
+let a = 138
+let b = 221
+let c = 399
+let d = 406
+let e = 575
+let f = 681
+let g = 745
+let h = 813
+let i = 955
+let j = 1000
+
+let average1 = (a + b + c + d + e) / 5
+console.log(average1)
+
+let average2 = (f + g + h + i + j) / 5
+console.log(average2)
+
+console.log((average1 + average2) / 2)
+
+
+// Find the average of ( 4, 10, 22, -30, 55);
+// Find the average of ( 68, 7, -22, 9, 100);
+// Find the average of the above;
+// Print in console
+
+let average3 = ((4 + 10 + 22 + 55 - 30) / 5)
+let average4 = ((68 + 7 - 22 + 9 + 100) / 5)
+
+console.log(average3)
+console.log(average4)
