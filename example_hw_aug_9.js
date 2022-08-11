24a8de22f47bb30304f1af990f553d5688f1757c
Remidemic <105177924+Remidemic@users.noreply.github.com>
2022-08-11 09:04:29 -0400

    Add files via upload

---
+++ example_hw_aug_9.js
@@ -0,0 +1,118 @@
+// let num = -10
+// console.log(num)
+
+// // if statement - is num positive or negative?
+
+// if (num === 0) {
+//     console.log("this number is just zero")
+// } else if (num > 0) {
+//     console.log("this number is positive")
+// } else {
+//     console.log("this number is negative")
+// }
+
+//-----------------------------------------------
+
+// let age = 2
+
+// if (age >= 18) {
+//     console.log("Access Granted!");
+// } else {
+//     console.log("Access Denied!");
+// }
+//----------------------------------------------
+
+// let num = -500
+// if (num > 0) {
+//     if (num >= 100) {
+//         console.log(num + " is greater than 100")
+//     } else if (num < 100) {
+//         console.log(num + " is less than 100 and Positive")
+//     }
+// } else {
+//     console.log(num + " is negative")
+// }
+//-----------------------------------------------
+
+// Write an if/else statement for the following requirements:
+
+// If student gets 80 or higher: console log You did a good job
+// If students get 70 or above: console log Keep trying
+// If students get 60 or above: console log ehhhh
+// If students get 55 or above: console log Not to good
+// Any grade lower than 55 is Bad Grade But You'll Get Em Next Time
+// Write a for loop for the given output: 10, 30, 50, 70, 90 20, 40, 60, 80, 100
+
+// This could be acheived a few ways, be creative.
+
+
+// let grade = 79
+
+// for (let grade = 10; grade <= 100; grade += 20) {
+
+//     if (grade >= 80) {
+//         console.log("you scored " + grade + " You did a good job")
+//     } else if (grade >= 70) {
+//         console.log("you scored " + grade + " Keep trying")
+//     } else if (grade >= 60) {
+//         console.log("you scored " + grade + " ehhhh")
+//     } else if (grade >= 55) {
+//         console.log("you scored " + grade + " Not too good")
+//     } else {
+//         console.log("you scored " + grade + " Bad Grade But You'll Get Em Next Time")
+//     }
+// }
+//-----------------------------------------------
+
+
+// //for loops 
+// for (let i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+
+// for (let i = 10; i > 0; i--) {
+//     console.log(i)
+// }
+
+// for (let i = 1; i < 10; i += 2) {
+//     console.log(i)
+// }
+
+// for (let i = 6; i < 61; i += 3) {
+//     console.log(i)
+// }
+
+//----------------------------------------------
+
+//Using the remainder % operator, write a for loop
+//that iterates between 0 and 10 and prints all even numbers.
+
+
+// for (let i = 1; i < 11; i++)
+//     if (i % 2 === 0) {
+//         console.log(i)
+//     }
+
+//----------------------------------------------
+
+// Write a program that prints the numbers from 1 to 100
+// and for multiples of '3' print "Fizz" instead of the number
+// and for the multiples of '5' print "Buzz".
+
+//****/ If can be multiplied by 3&5 console log 'FizzBuzz' ????????
+
+// for (let i = 1; i < 101; i++) {
+//     if (i % 15 === 0) {
+//         console.log("FizzBuzz")
+//     } else if (i % 3 === 0) {
+//         console.log("buzz")
+//     } else if (i % 5 === 0) {
+//         console.log("Buzz")
+//     } else {
+//         console.log(i)
+//     }
+// }
+//--------------------------------------------------------
+for (let i = 1; i < 10; i++) {
+    console.log(i = "#")
+}
